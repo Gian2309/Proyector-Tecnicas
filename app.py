9ce504596864e7df19ddc69b9f0b29db46b3d9c8
Gian2309 <n00390785@upn.pe>
2025-09-17 00:58:25 -0500

    Agregar app.py inicial

--- app.py
+++ app.py
@@ -1,5 +1,1 @@
-print('Hola desde Proyector-Tecnicas')
-print('Cambio distinto desde main')
-print('Nuevo cambio en feature')" 
-print('Cambio desde feature/mi-cambio') 
-
+print('Hola desde Proyector-Tecnicas') 
