6b9edcfb8587d15a1e69dc5db43e0a58a7ab1cc2
Gian Allcca <tu@correo.com>
2025-09-18 20:26:07 -0500

    chore(init): agregar README y app.py

--- app.py
+++ app.py
@@ -1,8 +1,1 @@
-print('Hola desde Proyector-Tecnicas') 
-print('Hola desde Proyector-Tecnicas') 
-print('Cambio distinto desde main') 
-print('Cambio desde feature/mi-cambio') 
-
-print('Cambio de la Semana 03') 
-print('Cambio de la Semana 03') 
-print('Ajuste por revisi¢n') 
+"print('Hola mundo')" 
